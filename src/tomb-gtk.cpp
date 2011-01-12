bd370c1ef5e23023cb4ce0177f507bc9daaa9963
Jaromil <jaromil@dyne.org>
2011-01-12 11:38:03 +0100

    more code to integrate gui elements and script

--- src/tomb-gtk.cpp
+++ src/tomb-gtk.cpp
@@ -1,105 +1,89 @@
+/*  Tomb - encrypted storage undertaker
+ *  
+ *  (c) Copyright 2007-2011 Denis Roio <jaromil@dyne.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 #include <gtk/gtk.h>
+#include <libnotify/notify.h>
 
 /* The Tomb icon is an artwork by Jordi aka MonMort
    a nomadic graffiti artist from Barcelona */
-const char *monmort[] = {
-/* columns rows colors chars-per-pixel */
-"32 32 5 1",
-"  c #000000",
-". c #010101",
-"X c #020202",
-"o c #C1C1C1",
-"O c None",
-/* pixels */
-"OO                            OO",
-"O oooooooooooooooooooooooooooo O",
-" oooooooooooooooooooooooooooooo ",
-" oooooooooooooooooooooooooooooo ",
-" oooooooo  X     oooooo     ooo ",
-" oooooooo        oooooo     ooo ",
-" oooooooo        oooooo     ooo ",
-" oooooooo        oooooo     oooX",
-" oooooooo    X   oooooo     ooo ",
-" oooooooo        oooooo     ooo ",
-" oooooooo       Xoooooo     ooo ",
-" oooooooo        oooooo   X ooo ",
-" oooooooo X      oooooo     ooo ",
-" oooooooooooooooooooooooooooooo ",
-" oooooooooooooooooooooo oooooooX",
-" ooooooooooooooooooooooo oooooo ",
-" oooooooooooooooo      X  ooooo ",
-"O ooooooooooooooooooooooooooooo ",
-"OO          oooooooooooooooooo O",
-"OOOOOOOOOOOO oooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo oooXooo OO",
-"OOOOOOOOOOOOO oooXooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooo ooo ooo oooXOO",
-"OOOOOOOOOOOOO ooo ooo ooo ooo OO",
-"OOOOOOOOOOOOOXooo ooo ooo ooo OO",
-"OOOOOOOOOOOOO ooooooooooooooo OO",
-"OOOOOOOOOOOOOO ooooooooooooo OOO",
-"OOOOOOOOOOOOOOO             OOOO"
-};
-
+#include <monmort.xpm>
 
 GtkStatusIcon *status_tomb;
 GtkMenu *menu_tomb;
 
+NotifyNotification *notice;
+GError *error;
+
 // forward declaration of callbacks
 gboolean left_click(GtkWidget *w, GdkEvent *e);
-gboolean cb_open(GtkWidget *w, GdkEvent *e);
-gboolean cb_quit(GtkWidget *w, GdkEvent *e);
-
+gboolean cb_close(GtkWidget *w, GdkEvent *e);
 
 
 int main(int argc, char **argv) {
-
   GObject *tray;
   GdkPixbuf *pb_monmort;
-  GtkWidget *menu_open, *menu_close, *menu_quit;
+  GtkWidget *menu_close;
   gint menu_x, menu_y;
   gboolean push_in = true;
 
+  char tomb_file[512];
+  
   gtk_set_locale();
   gtk_init(&argc, &argv);
+
+  // libnotify
+  notify_init(PACKAGE);
 
   // set and show the status icon
   pb_monmort = gdk_pixbuf_new_from_xpm_data(monmort);
   status_tomb = gtk_status_icon_new_from_pixbuf(pb_monmort);
   //  gtk_status_icon_set_name(status_tomb, "tomb");
   gtk_status_icon_set_title(status_tomb, "Tomb");
-  gtk_status_icon_set_tooltip_text (status_tomb, "Tomb - encrypted storage undertaker");
+  gtk_status_icon_set_tooltip_text (status_tomb, "Tomb - crypto undertaker");
 
   //  gtk_status_icon_set_blinking(status_tomb, true);
   menu_tomb = (GtkMenu*) gtk_menu_new();
 
-  menu_open = gtk_menu_item_new_with_label("Open");
-  gtk_menu_attach(menu_tomb, menu_open, 0, 1, 0, 1);
-  g_signal_connect_swapped(menu_open, "activate", G_CALLBACK(cb_open), NULL);
-  gtk_widget_show(menu_open);
-  
   menu_close = gtk_menu_item_new_with_label("Close");
-  gtk_menu_attach(menu_tomb, menu_close, 0, 1, 1, 2);
+  gtk_menu_attach(menu_tomb, menu_close, 0, 1, 0, 1);
+  g_signal_connect_swapped(menu_close, "activate", G_CALLBACK(cb_close), NULL);
   gtk_widget_show(menu_close);
-
-  menu_quit = gtk_menu_item_new_with_label("Quit");
-  gtk_menu_attach(menu_tomb, menu_quit, 0, 1, 2, 3);
-  g_signal_connect_swapped(menu_quit, "activate", G_CALLBACK(cb_quit), NULL);
-  gtk_widget_show(menu_quit);
-
+  
   g_signal_connect_swapped(status_tomb, "activate", G_CALLBACK(left_click), menu_tomb);
 
+  notice = notify_notification_new_with_status_icon
+    ("Tomb encrypted undertaker",
+     "We started digging out bones",
+     NULL, status_tomb);
+  notify_notification_set_icon_from_pixbuf(notice, pb_monmort);
+
+  notify_notification_show(notice, &error);
+
   gtk_main();
+  
+  notify_uninit();
 
   exit(0);
-  
   
 }
 
@@ -109,7 +93,7 @@ gboolean left_click(GtkWidget *w, GdkEvent *e) {
 		 gtk_status_icon_position_menu, status_tomb,
 		 1, gtk_get_current_event_time());
 } 
-gboolean cb_open(GtkWidget *w, GdkEvent *e) { 
-  execlp("tomb","tomb","mount","sarcofago","/mnt/etrom",NULL);
+gboolean cb_close(GtkWidget *w, GdkEvent *e) { 
+  execlp("tomb","tomb","-S","umount",NULL);
+  gtk_main_quit();
 }
-gboolean cb_quit(GtkWidget *w, GdkEvent *e) { gtk_main_quit(); }
