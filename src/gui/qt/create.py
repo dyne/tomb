4f1bb6401091a82beea0a15e3b63f1936d1f3c3e
BoySka <boyska@gmail.com>
2011-10-27 02:22:39 +0200

    Working draft (works bad)
    Password input is removed: not secure, tomb provides pinentry
    key location option is actually ignored. USB support is yet to come

--- src/gui/qt/create.py
+++ src/gui/qt/create.py
@@ -1,11 +1,12 @@
 import sys, os
 from PyQt4.QtGui import QApplication, QWizard
 from PyQt4 import QtCore
+from PyQt4 import QtGui
 from ui_create import Ui_Wizard
 
 parentdir = sys.path[0].split(os.sep)[:-1]
 sys.path.append(os.sep.join(parentdir))
-from tomblib import wrapper
+from tomblib import tomb
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -19,13 +20,23 @@ class TombCreateWizard(QWizard):
         ui.setupUi(self)
         
         ui.wizardPage_tomb_location.registerField('tombpath*', ui.lineEdit_tombpath) #required field, note the *
+        ui.wizardPage_key_location.setCommitPage(True)
 
-        QtCore.QObject.connect(self, QtCore.SIGNAL(_fromUtf8('finished(int)')), self.create_tomb)
+        QtCore.QObject.connect(ui.button_tombpath, QtCore.SIGNAL(_fromUtf8('clicked()')), self.on_tomb_location_clicked)
+        QtCore.QObject.connect(self, QtCore.SIGNAL(_fromUtf8('currentIdChanged(int)')), self.on_change_page)
 
+    def on_tomb_location_clicked(self, *args, **kwargs):
+        filename = QtGui.QFileDialog.getSaveFileName(self, 'Create Tomb', filter="*.tomb")
+        print filename
+        self.ui.lineEdit_tombpath.setText(filename)
+    def on_change_page(self, pagenumber):
+        if self.currentPage() == self.ui.wizardPage_progress:
+            self.create_tomb()
     def create_tomb(self, *args, **kwargs):
-        print 'creating'
-        wrapper.Tomb.create(1,2,3)
-        print 'created!'
+        #FIXME: this will lock up the GUI
+        #FIXME: no support for other keypath than "next to tomb"
+        tomb.Tomb.create(self.ui.lineEdit_tombpath.text(), str(self.ui.spinBox_size.value()), None)
+        self.ui.progressBar.setValue(100)
 
 
 if __name__ == '__main__':
