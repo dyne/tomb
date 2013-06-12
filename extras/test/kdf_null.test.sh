8d0c8e51e840744e02b8303599f85b6f4e6e0e7e
Jaromil <jaromil@dyne.org>
2013-06-12 13:36:50 +0200

    Brand new test suite
    this test suite can be called by 'make test' and checks basic functions
    plus KDF, STEGHIDE and RESIZE. It works creating files in /tmp/*tomb*

--- extras/test/kdf_null.test.sh
+++
@@ -1,9 +0,0 @@
-rm /tmp/kdf.tomb{,.key} -f || echo error removing previous files >&3
-sudo -k
-../tomb --no-color --unsecure-dev-mode --sudo-pwd $sudo_pwd --tomb-pwd f00za --use-urandom create /tmp/kdf -s 10 --kdf null >&4 2>&4 || echo "error creating (with --kdf null): $?" >&3
-egrep '^_KDF_' /tmp/kdf.tomb.key >&4 2>&4 && echo "error tomb kdf header present (--kdf=null), shouldn't" >&3
-sanity_tomb /tmp/kdf.tomb || echo error sanity checks: $? >&3
-rm /tmp/kdf.tomb{,.key} -f || echo error removing previous files >&3
-../tomb --no-color --unsecure-dev-mode --sudo-pwd $sudo_pwd --tomb-pwd f00za --use-urandom create /tmp/kdf -s 10 >&4 2>&4 || echo "error creating (without --kdf): $?" >&3
-egrep '^_KDF_' /tmp/kdf.tomb.key >&4 2>&4 && echo "error tomb kdf header present (no --kdf), shouldn't" >&3
-sanity_tomb /tmp/kdf.tomb || echo error sanity checks: $? >&3
