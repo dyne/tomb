4688820b68a2f098e72b1cfe26eebb7391a6b91c
Alexandre Pujol <alexandre@pujol.io>
2017-05-01 23:26:17 +0100

    Tests: support for /media and /usr/media

--- extras/test/50_integrity.sh
+++ extras/test/50_integrity.sh
@@ -7,12 +7,12 @@ source ./setup
 test_export "test" # Using already generated tomb
 test_expect_success 'Testing contents integrity' '
     tt_open --tomb-pwd $DUMMYPASS &&
-    tomb_set_ownership "$MEDIA/$USER/$testname" &&
-    tt dig -s 10 "$MEDIA/$USER/$testname/datacheck.raw" &&
-    CRC1=$(sha256sum "$MEDIA/$USER/$testname/datacheck.raw") &&
+    tomb_set_ownership "$MEDIA/$testname" &&
+    tt dig -s 10 "$MEDIA/$testname/datacheck.raw" &&
+    CRC1=$(sha256sum "$MEDIA/$testname/datacheck.raw") &&
     tt_close --unsafe &&
     tt_open --tomb-pwd $DUMMYPASS &&
-    CRC2=$(sha256sum "$MEDIA/$USER/$testname/datacheck.raw") &&
+    CRC2=$(sha256sum "$MEDIA/$testname/datacheck.raw") &&
     [[ "$CRC1" == "$CRC2" ]] &&
     tt_close
     '
