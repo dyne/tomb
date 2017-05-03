593747d61915327f69336ea72a6a1c5060962ad5
Alexandre Pujol <alexandre@pujol.io>
2017-05-03 19:51:36 +0100

    Add bind hooks tests and minors test setup changes

--- extras/test/50_integrity.sh
+++ extras/test/50_integrity.sh
@@ -7,7 +7,7 @@ source ./setup
 test_export "test" # Using already generated tomb
 test_expect_success 'Testing contents integrity' '
     tt_open --tomb-pwd $DUMMYPASS &&
-    tomb_set_ownership "$MEDIA/$testname" &&
+    tt_set_ownership "$MEDIA/$testname" &&
     tt dig -s 10 "$MEDIA/$testname/datacheck.raw" &&
     CRC1=$(sha256sum "$MEDIA/$testname/datacheck.raw") &&
     tt_close --unsafe &&
