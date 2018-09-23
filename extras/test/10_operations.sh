aaa4637ed079b140cbe0298128b42c19f44c8166
Alexandre Pujol <alexandre@pujol.io>
2018-09-23 22:21:05 +0100

    Track access/modification time of sensitive files
    Collects the stats of tomb keys and tomb files then restore them when
    Tomb exits. Can be extended to any file opened by Tomb. See #266

--- extras/test/10_operations.sh
+++ extras/test/10_operations.sh
@@ -4,6 +4,9 @@ export test_description="Testing common operations on tomb"
 
 source ./setup
 
+_getaccess() { stat --format=%X "$1"; }
+_getmodif() { stat --format=%Y "$1"; }
+
 test_export "test" # Using already generated tomb
 test_expect_success 'Testing open with wrong password ' '
     test_must_fail tt_open --tomb-pwd wrongpassword
@@ -19,6 +22,15 @@ test_expect_success 'Testing open in read only mode' '
     tt_open --tomb-pwd $DUMMYPASS -o ro,noatime,nodev &&
     tt_close &&
     chmod +w $tomb
+    '
+
+test_expect_success 'Testing tomb files stat restoration' '
+    access=$(_getaccess $tomb_key) &&
+    modif=$(_getmodif $tomb_key) &&
+    tt_open --tomb-pwd $DUMMYPASS &&
+    tt_close &&
+    [[ "$access" == "$(_getaccess $tomb_key)" ]] &&
+    [[ "$modif" == "$(_getmodif $tomb_key)" ]]
     '
 
 if test_have_prereq LSOF; then
