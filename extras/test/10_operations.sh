61386ca64659af44eaefb645b4d0a7db4f03b667
heat-wave <allpicksucks@gmail.com>
2021-04-07 18:20:12 +0100

    Support for sudo alternatives such as doas

--- extras/test/10_operations.sh
+++ extras/test/10_operations.sh
@@ -52,5 +52,12 @@ if test_have_prereq SPHINX ORACLE; then
         '
 fi
 
+if test_have_prereq DOAS; then
+    test_export "doas_test" # Using already generated tomb
+    test_expect_success 'Testing open with good password (using doas instead of sudo)' '
+        tt_open --sudo doas --tomb-pwd $DUMMYPASS &&
+        tt_close
+        '
+fi
 
 test_done
